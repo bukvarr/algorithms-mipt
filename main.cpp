edbd25feb708f06a27fd6df3776fc5f22d407cca
bukvarr <danisimo07o7@gmail.com>
2022-10-30 23:47:27 +0300

    1_7 changes

--- main.cpp
+++ main.cpp
@@ -4,29 +4,29 @@
 
 #include <iostream>
 
-class Prefixes {
+class PrefixArray {
  public:
   int size;
-  double RequestProcessing();
   double* prefix_array;
-  void ArrayConstructing(int index, double t);
-  ~Prefixes() { delete[] prefix_array; }
+  void InsertGM(int index, double t);
+  double GetGM(int left_index, int right_index);
+  PrefixArray(int size) : size(size), prefix_array(new double[size]){};
+  ~PrefixArray() { delete[] prefix_array; }
 };
 
-double Prefixes::RequestProcessing() {
-  int l, r;
-  std::cin >> l >> r;
+double PrefixArray::GetGM(int left_index, int right_index) {
   double product;
-  if (l == 0) {
-    product = prefix_array[r];
+  if (left_index == 0) {
+    product = prefix_array[right_index];
   } else {
-    product = prefix_array[r] / prefix_array[l - 1];
+    product = prefix_array[right_index] / prefix_array[left_index - 1];
   }
-  double t = pow(product, static_cast<double>(size) / (double)(r - l + 1));
+  double t = pow(product, static_cast<double>(size) /
+                          (double)(right_index - left_index + 1));
   return t;
 }
 
-void Prefixes::ArrayConstructing(int index, double t) {
+void PrefixArray::InsertGM(int index, double t) {
   if (index == 0) {
     prefix_array[0] = pow(t, (double)1 / static_cast<double>(size));
     return;
@@ -36,18 +36,20 @@ void Prefixes::ArrayConstructing(int index, double t) {
 }
 
 int main() {
-  Prefixes prefix_products;
-  std::cin >> prefix_products.size;
-  prefix_products.prefix_array = new double[prefix_products.size];
+  int size;
+  std::cin >> size;
+  PrefixArray prefix_products(size);
   for (int i = 0; i < prefix_products.size; ++i) {
     double t;
     std::cin >> t;
-    prefix_products.ArrayConstructing(i, t);
+    prefix_products.InsertGM(i, t);
   }
   int q;
   std::cin >> q;
   for (int i = 0; i < q; ++i) {
-    double ans = prefix_products.RequestProcessing();
+    int left_index, right_index;
+    std::cin >> left_index >> right_index;
+    double ans = prefix_products.GetGM(left_index, right_index);
     std::cout << std::fixed << std::setprecision(10) << ans << '\n';
   }
 }
