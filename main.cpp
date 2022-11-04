11b48b4bde6202dca5732e5acb3dce88ad4c4514
bukvarr <danisimo07o7@gmail.com>
2022-11-04 11:17:31 +0300

    2_7 changes

--- main.cpp
+++ main.cpp
@@ -4,70 +4,76 @@
 
 #include <iostream>
 
-class SortingTools {
-public:
-		int size;
-		unsigned long long* numbers = new unsigned long long[1'000'000];
-		unsigned long long* sorted_numbers = new unsigned long long[1'000'000];
-		unsigned long long* bytes_array = new unsigned long long[1'000'000];
-		void Input();
-		void ByteSorting(const int& byte);
-		void LsdSorting();
-		void Output();
-		unsigned long long FindByte(unsigned long long num, const int& byte);
+class BigNumbersArray {
+ private:
+  int size_;
+  unsigned long long *numbers_;
+  unsigned long long *sorted_numbers_;
+  unsigned long long *bytes_array_;
+
+  void ByteSorting(const int& byte) {
+    int counting_array[256] = {};
+    for (int i = 0; i < size_; ++i) {
+      bytes_array_[i] = FindByte(numbers_[i], byte);
+      ++counting_array[bytes_array_[i]];
+    }
+    for (int i = 1; i < 256; ++i) {
+      counting_array[i] += counting_array[i - 1];
+    }
+    for (int i = size_ - 1; i >= 0; --i) {
+      sorted_numbers_[--counting_array[bytes_array_[i]]] = numbers_[i];
+    }
+    for (int i = 0; i < size_; ++i) {
+      numbers_[i] = sorted_numbers_[i];
+    }
+  }
+
+  unsigned long long FindByte(unsigned long long num,
+                                               const int& byte) {
+    num = num >> (byte << 3);
+    return num % 256;
+  }
+
+ public:
+  BigNumbersArray(int size) : size_(size), numbers_(new unsigned long long[size_]),
+                              sorted_numbers_(new unsigned long long[size_]),
+                              bytes_array_(new unsigned long long[size_]){};
+
+  void Sort() {
+    for (int i = 0; i < 8; ++i) {
+      ByteSorting(i);
+    }
+  }
+
+  void ArrayConstricting(unsigned long long t, int index) {
+    numbers_[index] = t;
+  }
+
+  unsigned long long GiveNumber(int index) {
+    return sorted_numbers_[index];
+  }
+
+  ~BigNumbersArray() {
+    delete[] numbers_;
+    delete[] sorted_numbers_;
+    delete[] bytes_array_;
+  }
 };
 
-void SortingTools::Input() {
-	for (int i = 0; i < size; ++i) {
-		std::cin >> numbers[i];
-	}
-}
-
-unsigned long long SortingTools::FindByte(unsigned long long num,
-                                          const int& byte) {
-	num = num >> (byte << 3);
-	return num % 256;
-}
-
-void SortingTools::ByteSorting(const int& byte) {
-	int counting_array[256] = {};
-	for (int i = 0; i < size; ++i) {
-		bytes_array[i] = FindByte(numbers[i], byte);
-		++counting_array[bytes_array[i]];
-	}
-	for (int i = 1; i < 256; ++i) {
-		counting_array[i] += counting_array[i - 1];
-	}
-	for (int i = size - 1; i >= 0; --i) {
-		sorted_numbers[--counting_array[bytes_array[i]]] = numbers[i];
-	}
-	for (int i = 0; i < size; ++i) {
-		numbers[i] = sorted_numbers[i];
-	}
-}
-
-void SortingTools::LsdSorting() {
-	for (int i = 0; i < 8; ++i) {
-		ByteSorting(i);
-	}
-}
-
-void SortingTools::Output() {
-	for (int i = 0; i < size; ++i) {
-		std::cout << numbers[i] << " ";
-	}
-}
-
 int main() {
-	std::ios_base::sync_with_stdio(false); //speeding up cin
-	std::basic_ostream<char, std::char_traits<char> >* aboba = 0; //speeding up cin
-	std::cin.tie(aboba); //speeding up cin
-	SortingTools sort;
-	std::cin >> sort.size;
-	sort.Input();
-	sort.LsdSorting();
-	sort.Output();
-	delete[] sort.numbers;
-	delete[] sort.sorted_numbers;
-	delete[] sort.bytes_array;
+  std::ios_base::sync_with_stdio(false); //speeding up cin
+  std::basic_ostream<char, std::char_traits<char> >* aboba = 0; //speeding up cin
+  std::cin.tie(aboba); //speeding up cin
+  int size;
+  std::cin >> size;
+  BigNumbersArray array(size);
+  for (int i = 0; i < size; ++i) {
+    unsigned long long t;
+    std::cin >> t;
+    array.ArrayConstricting(t, i);
+  }
+  array.Sort();
+  for (int i = 0; i < size; ++i) {
+    std::cout << array.GiveNumber(i) << '\n';
+  }
 }
