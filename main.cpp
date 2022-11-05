f7ccd0eca163627e73f93364a5c323787e39dd06
bukvarr <danisimo07o7@gmail.com>
2022-11-05 19:30:40 +0300

    2_8 changes

--- main.cpp
+++ main.cpp
@@ -1,45 +1,59 @@
+//Нужно с помощью быстрой сортировки отсортировать массив.
+
+//Запрещено инклудить algorithm!
+
 #include <iostream>
 
-int Partition(int* a, int left_border, int right_border) {
+int Partition(int* array, int pivot, int left_border, int right_border) {
   int left_index = left_border;
   int right_index = right_border - 1;
-  int pivot = a[right_index];
-  while (left_index != right_index) {
-    if (a[left_index] > pivot && a[right_index] <= pivot) {
-      a[left_index] += a[right_index];
-      a[right_index] = a[left_index] - a[right_index];
-      a[left_index] -= a[right_index];
+  while (right_index != left_index) {
+    if (array[left_index] >= pivot && array[right_index] <= pivot) {
+      std::swap(array[left_index], array[right_index]);
     }
-    if (a[left_index] <= pivot) {
+    if (array[left_index] <= pivot) {
       ++left_index;
-    } else if (a[right_index] >= pivot) {
+    }
+    if (pivot <= array[right_index] && right_index != left_index) {
       --right_index;
     }
+  }
+  if (array[left_index] < pivot) {
+    return left_index + 1;
   }
   return left_index;
 }
 
-void QuickSort(int*a, int left_border, int right_border) {
-  int p = Partition(a, left_border, right_border);
+void QuickSort(int* array, int left_border, int right_border) {
+  if (right_border == left_border) {
+    return;
+  }
+  int pivot;
+  int random = 0;
+  if (right_border > left_border) {
+    random = std::rand() % (right_border - left_border);
+  }
+  pivot = array[left_border + random];
+  int p = Partition(array, pivot, left_border, right_border);
   if (right_border - left_border <= 2) {
     return;
   }
-  QuickSort(a, left_border, p);
-  QuickSort(a, p, right_border);
+  QuickSort(array, left_border, p);
+  QuickSort(array, p, right_border);
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  int a[100];
-  for (int i = 0; i < n; ++i) {
+  int size;
+  std::cin >> size;
+  int* array = new int[size];
+  for (int i = 0; i < size; ++i) {
     int t;
     std::cin >> t;
-    a[i] = t;
+    array[i] = t;
   }
-  QuickSort(a, 0, n);
-  for (int i = 0; i < n; ++i) {
-    std::cout << a[i] << " ";
+  QuickSort(array, 0, size);
+  for (int i = 0; i < size; ++i) {
+    std::cout << array[i] << std::endl;
   }
+  delete[] array;
 }
-
