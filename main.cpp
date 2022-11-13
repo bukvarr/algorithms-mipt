27909c91ebea7393e603e98b70cdf6ca1e83adb8
bukvarr <danisimo07o7@gmail.com>
2022-11-14 00:42:23 +0300

    2_3 changes

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-/* Реализуйте двоичную кучу.
-
-Обработайте запросы следующих видов:
-
-    insert x — вставить целое число x в кучу;
-
-    getMin — вывести значение минимального элемента в куче (гарантируется, что к этому моменту куча не пуста);
-
-    extractMin — удалить минимальный элемент из кучи, выводить его не нужно (гарантируется, что к этому моменту куча не пуста);
-
-    decreaseKey i Δ — уменьшить число, вставленное на i-м запросе, на целое число Δ≥0
-    (гарантируется, что i-й запрос был осуществлён ранее, являлся запросом добавления, а добавленное на этом шаге число всё ещё лежит в куче).
-    Обратите внимание, число i равно номеру запроса среди всех запросов, а не только среди запросов добавления!
-
-Можете считать, что в любой момент времени все числа, хранящиеся в куче, попарно различны, а их количество не превышает 100'000.
-*/
-
 #include <iostream>
 
 class Heap {
  private:
   std::pair<long long, int> heap_array_[100'000];
-  int requests_order_[1'000'000];
+  int* requests_order_;
   int size_ = 0;
 
   void SiftDown(int index) {
@@ -54,6 +37,8 @@ class Heap {
   }
 
  public:
+  Heap(int rq) : requests_order_(new int[rq]) {}
+
   void Insert(long long x, int request_number) {
     requests_order_[request_number] = size_;
     heap_array_[size_].first = x;
@@ -63,10 +48,7 @@ class Heap {
 
   long long GetMin() const { return heap_array_[0].first; }
 
-  void DecreaseKey() {
-    int request_number;
-    long long delta;
-    std::cin >> request_number >> delta;
+  void DecreaseKey(int request_number, long long delta) {
     int index = requests_order_[request_number];
     heap_array_[index].first -= delta;
     SiftUp(index);
@@ -78,35 +60,35 @@ class Heap {
               requests_order_[heap_array_[size_].second]);
     SiftDown(0);
   }
+
+  ~Heap() { delete[] requests_order_; }
 };
 
-void RequestProcessing(std::string s, int request_number, Heap& heap) {
-  if (s == "insert") {
-    long long x;
-    std::cin >> x;
-    heap.Insert(x, request_number);
-  }
-  if (s == "decreaseKey") {
-    heap.DecreaseKey();
-  }
-  if (s == "extractMin") {
-    heap.ExtractMin();
-  }
-}
-
 int main() {
-  Heap heap;
   std::ios_base::sync_with_stdio(false);
   std::basic_ostream<char, std::char_traits<char> >* aboba = 0;
   std::cin.tie(aboba);
   int q;
   std::cin >> q;
+  Heap heap(q + 1);
   for (int i = 1; i <= q; ++i) {
     std::string s;
     std::cin >> s;
-    if (s != "getMin") {
-      RequestProcessing(s, i, heap);
-    } else {
+    if (s == "insert") {
+      long long x;
+      std::cin >> x;
+      heap.Insert(x, i);
+    }
+    if (s == "decreaseKey") {
+      int request_number;
+      long long delta;
+      std::cin >> request_number >> delta;
+      heap.DecreaseKey(request_number, delta);
+    }
+    if (s == "extractMin") {
+      heap.ExtractMin();
+    }
+    if (s == "getMin") {
       std::cout << heap.GetMin() << '\n';
     }
   }
