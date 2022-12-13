c3db721420eeeb7e443460a8fe4757afcbf16996
bukvarr <danisimo07o7@gmail.com>
2022-12-14 00:13:58 +0300

    2_3 changes

--- main.cpp
+++ main.cpp
@@ -7,67 +7,82 @@ struct Node {
 
 class Heap {
  private:
-  Node heap_array_[100'000];
+  Node* heap_array_;
   int* requests_order_;
   int size_ = 0;
 
-  void SiftDown(int index) {
-    while (2 * index + 1 < size_) {
-      int u = 2 * index + 1;
-      if (u + 1 < size_ && heap_array_[u + 1].value < heap_array_[u].value) {
-        ++u;
-      }
-      if (heap_array_[u].value < heap_array_[index].value) {
-        std::swap(heap_array_[index], heap_array_[u]);
-        std::swap(requests_order_[heap_array_[index].request_number],
-                  requests_order_[heap_array_[u].request_number]);
-        index = u;
-      } else {
-        break;
-      }
-    }
-  }
+  void SiftDown(int index);
 
-  void SiftUp(int index) {
-    while (index > 0) {
-      if (heap_array_[index].value < heap_array_[(index - 1) / 2].value) {
-        std::swap(heap_array_[index], heap_array_[(index - 1) / 2]);
-        std::swap(requests_order_[heap_array_[index].request_number],
-                  requests_order_[heap_array_[(index - 1) / 2].request_number]);
-        index = (index - 1) / 2;
-      } else {
-        break;
-      }
-    }
-  }
+  void SiftUp(int index);
 
  public:
-  Heap(int rq) : requests_order_(new int[rq]) {}
+  Heap(int rq)
+          : heap_array_(new Node[std::min(rq, 100'000)]),
+            requests_order_(new int[rq]) {}
 
-  void Insert(long long x, int request_number) {
-    requests_order_[request_number] = size_;
-    heap_array_[size_].value = x;
-    heap_array_[size_].request_number = request_number;
-    SiftUp(size_++);
-  }
+  void Insert(long long x, int request_number);
 
   long long GetMin() const { return heap_array_[0].value; }
 
-  void DecreaseKey(int request_number, long long delta) {
-    int index = requests_order_[request_number];
-    heap_array_[index].value -= delta;
-    SiftUp(index);
-  }
+  void DecreaseKey(int request_number, long long delta);
 
-  void ExtractMin() {
-    std::swap(heap_array_[0], heap_array_[--size_]);
-    std::swap(requests_order_[heap_array_[0].request_number],
-              requests_order_[heap_array_[size_].request_number]);
-    SiftDown(0);
-  }
+  void ExtractMin();
 
-  ~Heap() { delete[] requests_order_; }
+  ~Heap() {
+    delete[] heap_array_;
+    delete[] requests_order_;
+  }
 };
+
+void Heap::SiftDown(int index) {
+  while (2 * index + 1 < size_) {
+    int u = 2 * index + 1;
+    if (u + 1 < size_ && heap_array_[u + 1].value < heap_array_[u].value) {
+      ++u;
+    }
+    if (heap_array_[u].value < heap_array_[index].value) {
+      std::swap(heap_array_[index], heap_array_[u]);
+      std::swap(requests_order_[heap_array_[index].request_number],
+                requests_order_[heap_array_[u].request_number]);
+      index = u;
+    } else {
+      break;
+    }
+  }
+}
+
+void Heap::SiftUp(int index) {
+  while (index > 0) {
+    if (heap_array_[index].value < heap_array_[(index - 1) / 2].value) {
+      std::swap(heap_array_[index], heap_array_[(index - 1) / 2]);
+      std::swap(requests_order_[heap_array_[index].request_number],
+                requests_order_[heap_array_[(index - 1) / 2].request_number]);
+      index = (index - 1) / 2;
+    } else {
+      break;
+    }
+  }
+}
+
+void Heap::Insert(long long x, int request_number) {
+  requests_order_[request_number] = size_;
+  heap_array_[size_].value = x;
+  heap_array_[size_].request_number = request_number;
+  SiftUp(size_++);
+}
+
+void Heap::DecreaseKey(int request_number, long long delta) {
+  int index = requests_order_[request_number];
+  heap_array_[index].value -= delta;
+  SiftUp(index);
+}
+
+void Heap::ExtractMin() {
+  std::swap(heap_array_[0], heap_array_[--size_]);
+  std::swap(requests_order_[heap_array_[0].request_number],
+            requests_order_[heap_array_[size_].request_number]);
+  SiftDown(0);
+}
 
 int main() {
   std::ios_base::sync_with_stdio(false);
