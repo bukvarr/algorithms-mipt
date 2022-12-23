b85dd2222946df6e59e5928b48551bb1e79a1172
bukvarr <danisimo07o7@gmail.com>
2022-12-23 17:48:14 +0300

    4_1 changes

--- main.cpp
+++ main.cpp
@@ -2,77 +2,81 @@
 
 #include <iostream>
 
-#include <vector>
-
 class Hashmap {
- private:
-  const unsigned int kN = 1'000'000'000;
-  unsigned int cnt_ = 0;
-  unsigned int* array_ = new unsigned int[4 * 1024 * 1024];
-  unsigned int* count_ = new unsigned int[4 * 1024 * 1024];
-  unsigned int Hash1(unsigned int x) { return x % (4 * 1024 * 1024); }
-  unsigned int Hash2(unsigned int x) {
-    return ((x % 997) * 2 + 1) % (4 * 1024 * 1024);
-  }
-
  public:
-  Hashmap() {
-    std::fill(array_, array_ + (4 * 1024 * 1024), kN + 1);
-    std::fill(count_, count_ + (4 * 1024 * 1024), 0);
-  }
+  Hashmap(unsigned int max_key, unsigned int size);
 
-  void Insert(unsigned int x) {
-    unsigned int probe_point = Hash1(x);
-    unsigned int inc = Hash2(x);
-    while (array_[probe_point] < (kN + 1) && array_[probe_point] != x) {
-      probe_point = (probe_point + inc) % (4 * 1024 * 1024);
-    }
-    array_[probe_point] = x;
-    ++count_[probe_point];
-  }
+  void Insert(unsigned int x);
 
-  bool Extract(unsigned int x) {
-    unsigned int probe_point = Hash1(x);
-    unsigned int inc = Hash2(x);
-    while (array_[probe_point] != (kN + 1) && array_[probe_point] != x) {
-      probe_point = (probe_point + inc) % (4 * 1024 * 1024);
-    }
-    if (array_[probe_point] == x && count_[probe_point] != 0) {
-      ++cnt_;
-      --count_[probe_point];
-      return true;
-    }
-    return false;
-  }
+  bool FindEntry(unsigned int x);
 
-  unsigned int Cnt() { return cnt_; }
+  void Erase(unsigned int x);
 
-  ~Hashmap() {
-    delete[] array_;
-    delete[] count_;
-  }
+  ~Hashmap() { delete[] array_; }
+
+ private:
+  unsigned int max_key_;
+  unsigned int size_;
+  unsigned int* array_;
+  unsigned int Hash1(unsigned int x) { return x % (size_); }
+  unsigned int Hash2(unsigned int x) { return ((x % 1997) * 2 + 1) % (size_); }
 };
 
+Hashmap::Hashmap(unsigned int max_key, unsigned int size)
+        : max_key_(max_key), size_(size), array_(new unsigned int[size]) {
+  std::fill(array_, array_ + (size_), max_key_ + 1);
+}
+
+void Hashmap::Insert(unsigned int x) {
+  unsigned int probe_point = Hash1(x);
+  unsigned int inc = Hash2(x);
+  while (array_[probe_point] < (max_key_ + 1) && array_[probe_point] != x) {
+    probe_point = (probe_point + inc) % (size_);
+  }
+  array_[probe_point] = x;
+}
+
+bool Hashmap::FindEntry(unsigned int x) {
+  unsigned int probe_point = Hash1(x);
+  unsigned int inc = Hash2(x);
+  while (array_[probe_point] != (max_key_ + 1) && array_[probe_point] != x) {
+    probe_point = (probe_point + inc) % (size_);
+  }
+  return (array_[probe_point] == x);
+}
+
+void Hashmap::Erase(unsigned int x) {
+  unsigned int probe_point = Hash1(x);
+  unsigned int inc = Hash2(x);
+  while (array_[probe_point] != (max_key_ + 1) && array_[probe_point] != x) {
+    probe_point = (probe_point + inc) % (size_);
+  }
+  if (array_[probe_point] == x) {
+    array_[probe_point] = max_key_ + 2;
+  }
+}
+
 int main() {
-  Hashmap hashmap;
+  Hashmap hashmap(1'000'000'000, 1024 * 1024);
   int q;
   std::cin >> q;
   for (int i = 0; i < q; ++i) {
+    char c;
+    std::cin >> c;
     unsigned int x;
     std::cin >> x;
-    hashmap.Insert(x);
-  }
-  std::cin >> q;
-  std::vector<unsigned int> vec;
-  for (int i = 0; i < q; ++i) {
-    unsigned int x;
-    std::cin >> x;
-    if (hashmap.Extract(x)) {
-      vec.push_back(x);
+    if (c == '+') {
+      hashmap.Insert(x);
+    }
+    if (c == '-') {
+      hashmap.Erase(x);
+    }
+    if (c == '?') {
+      if (hashmap.FindEntry(x)) {
+        std::cout << "YES" << '\n';
+      } else {
+        std::cout << "NO" << '\n';
+      }
     }
   }
-  std::cout << hashmap.Cnt() << std::endl;
-  for (size_t i = 0; i < vec.size(); ++i) {
-    std::cout << vec[i] << " ";
-  }
-} 
+}
