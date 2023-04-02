dc9ba5903ae8474b340a9c497bfbe94eb568ae0b
bukvarr <danisimo07o7@gmail.com>
2023-04-02 17:19:06 +0300

    changes

--- 6_K/main.cpp
+++ 6_K/main.cpp
@@ -2,20 +2,20 @@
 
 #include <vector>
 
-void DynamicConstructing(std::vector<std::vector<int>>& dp,
+void ConstructingDP(std::vector<std::vector<int>>& dp,
                          const std::vector<int>& weight,
                          const std::vector<int>& cost) {
-  int n = static_cast<int>(dp.size());
-  int m = static_cast<int>(dp[0].size()) - 1;
-  for (int j = 0; j < weight[0]; ++j) {
+  size_t n = static_cast<int>(dp.size());
+  size_t m = static_cast<int>(dp[0].size()) - 1;
+  for (size_t j = 0; j < weight[0]; ++j) {
     dp[0][j] = 0;
   }
-  for (int j = weight[0]; j <= m; ++j) {
+  for (size_t j = weight[0]; j <= m; ++j) {
     dp[0][j] = cost[0];
   }
-  for (int i = 1; i < n; ++i) {
-    for (int j = 0; j <= m; ++j) {
-      dp[i][j] = (j >= weight[i] ? std::max(dp[i - 1][j - weight[i]] + cost[i],
+  for (size_t i = 1; i < n; ++i) {
+    for (size_t j = 0; j <= m; ++j) {
+      dp[i][j] = (j >= weight[i] ? std::max(dp[i - 1][j - weight[i]] + cost[i], //updating max cost
                                             dp[i - 1][j])
                                  : dp[i - 1][j]);
     }
@@ -24,16 +24,16 @@ void DynamicConstructing(std::vector<std::vector<int>>& dp,
 
 std::vector<int> IndexesGathering(const std::vector<std::vector<int>>& dp,
                                   const std::vector<int>& weight) {
-  int n = static_cast<int>(dp.size());
-  int m = static_cast<int>(dp[0].size()) - 1;
-  int max_w = 0;
-  for (int j = 1; j <= m; ++j) {
+  size_t n = static_cast<int>(dp.size());
+  size_t m = static_cast<int>(dp[0].size()) - 1;
+  size_t max_w = 0;
+  for (size_t j = 1; j <= m; ++j) {
     if (dp[n - 1][j] > dp[n - 1][max_w]) {
       max_w = j;
     }
   }
   std::vector<int> indexes;
-  for (int i = n - 1; i >= 0; --i) {
+  for (size_t i = n - 1; i >= 0; --i) {
     if (i == 0) {
       if (dp[0][max_w] != 0) {
         indexes.push_back(1);
@@ -49,7 +49,7 @@ std::vector<int> IndexesGathering(const std::vector<std::vector<int>>& dp,
 }
 
 int main() {
-  int n, m;
+  size_t n, m;
   std::cin >> n >> m;
   std::vector<int> weight(n);
   std::vector<int> cost(n);
@@ -60,7 +60,7 @@ int main() {
     std::cin >> cost[i];
   }
   std::vector<std::vector<int>> dp(n, std::vector<int>(m + 1, 0));
-  DynamicConstructing(dp, weight, cost);
+  ConstructingDP(dp, weight, cost);
   std::vector<int> indexes = IndexesGathering(dp, weight);
   for (size_t i = indexes.size(); i >= 1; --i) {
     std::cout << indexes[i - 1] << '\n';
