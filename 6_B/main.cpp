3ffb91fbfb124b72c80b067e1a64df890299763a
bukvarr <danisimo07o7@gmail.com>
2023-04-02 16:25:23 +0300

    changes

--- 6_B/main.cpp
+++ 6_B/main.cpp
@@ -2,21 +2,23 @@
 
 #include <vector>
 
-void DynamicConstruction(const std::vector<long long>& a, std::vector<int>& dp,
-                         std::vector<int>& max_pos) {
-  size_t n = a.size();
+void ConstructingDP(const std::vector<long long>& nums, std::vector<int>& dp,
+                    std::vector<size_t>& max_pos) {
+  dp[0] = 0;
+  max_pos[0] = 0;
+  size_t n = nums.size();
   for (size_t i = 1; i < n; ++i) {
     size_t l = 0;
     size_t r = n;
-    while (r - l > 1) {
+    while (r - l > 1) { // bin search of new pos
       size_t m = (l + r) / 2;
-      if (dp[m] == -1 || a[dp[m]] < a[i]) {
+      if (dp[m] == -1 || nums[dp[m]] < nums[i]) {
         r = m;
       } else {
         l = m;
       }
     }
-    if (a[dp[l]] >= a[i]) {
+    if (nums[dp[l]] >= nums[i]) { //updating pos
       dp[l + 1] = i;
       max_pos[i] = l + 1;
     } else {
@@ -28,11 +30,11 @@ void DynamicConstruction(const std::vector<long long>& a, std::vector<int>& dp,
   }
 }
 
-std::vector<int> IndexesGathering(const std::vector<int>& dp,
-                                  const std::vector<int>& max_pos) {
-  int n = static_cast<int>(dp.size());
-  int length = 0;
-  for (int i = n - 1; i > 0; --i) {
+std::vector<int> GatheringIndexes(const std::vector<int>& dp,
+                                  const std::vector<size_t>& max_pos) {
+  size_t n = dp.size();
+  size_t length = 0;
+  for (size_t i = n - 1; i > 0; --i) {
     if (dp[i] != -1) {
       length = i;
       break;
@@ -40,12 +42,15 @@ std::vector<int> IndexesGathering(const std::vector<int>& dp,
   }
   int index = dp[length];
   std::vector<int> vect;
-  while (length >= 0) {
+  while (true) {
     if (max_pos[index] == length) {
       vect.push_back(index + 1);
       --length;
     }
     --index;
+    if (length == static_cast<size_t>(-1)) {
+      break;
+    }
   }
   return vect;
 }
@@ -53,16 +58,14 @@ std::vector<int> IndexesGathering(const std::vector<int>& dp,
 int main() {
   size_t n;
   std::cin >> n;
-  std::vector<long long> a(n);
+  std::vector<long long> nums(n);
   std::vector<int> dp(n, -1); //on each iteration max sequence on first i elements with length = 1, ... , n
-  std::vector<int> max_pos(n, -1); //max position of element 0 <= i < n
+  std::vector<size_t> max_pos(n, -1); //max position of element 0 <= i < n
   for (size_t i = 0; i < n; ++i) {
-    std::cin >> a[i];
+    std::cin >> nums[i];
   }
-  dp[0] = 0;
-  max_pos[0] = 0;
-  DynamicConstruction(a, dp, max_pos);
-  std::vector<int> indexes = IndexesGathering(dp, max_pos);
+  ConstructingDP(nums, dp, max_pos);
+  std::vector<int> indexes = GatheringIndexes(dp, max_pos);
   std::cout << indexes.size() << '\n';
   for (int i = indexes.size() - 1; i >= 0; --i) {
     std::cout << indexes[i] << " ";
