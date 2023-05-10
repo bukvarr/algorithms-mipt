7f54f26dabf42d9e4a5a119ef8e673a8569859de
danis.shamsutdinov <danisimo07o7@gmail.com>
2023-05-10 23:48:25 +0300

    changes

--- 8-1/main.cpp
+++ 8-1/main.cpp
@@ -50,23 +50,10 @@ struct Comp {
 };
 
 class Graph {
- private:
-  int vert_num_;
-  std::vector<std::vector<Edge>> edge_lists_;
-  std::vector<long long> dists_;
-  std::set<Node, Comp> unused_;
-
  public:
-  Graph(int n)
-          : vert_num_(n),
-            edge_lists_(std::vector<std::vector<Edge>>(n)),
-            dists_(std::vector<long long>(vert_num_, kMaxDist)) {
-    for (int i = 0; i < vert_num_; ++i) {
-      unused_.insert(Node(i, dists_[i]));
-    }
-  }
+  Graph(int n) : vert_num_(n), edge_lists_(std::vector<std::vector<Edge>>(n)) {}
 
-  Graph() = default;
+  Graph() = delete;
 
   void AddEdge(int v1, Edge ee) { edge_lists_[v1].push_back(ee); }
 
@@ -76,23 +63,9 @@ class Graph {
 
   int VerticesNum() const { return vert_num_; }
 
-  void DistUpd(int vert, long long new_dist) {
-    unused_.erase(Node(vert, dists_[vert]));
-    dists_[vert] = new_dist;
-    unused_.insert(Node(vert, dists_[vert]));
-  }
-
-  int GetDist(int vv) { return dists_[vv]; }
-
-  bool UnusedExists() { return !(unused_.empty()); }
-
-  int GetNearestUnused() {
-    auto it = unused_.begin();
-    Node nearest = *it;
-    unused_.erase(it);
-    dists_[nearest.vert] = nearest.dist;
-    return nearest.vert;
-  }
+ private:
+  int vert_num_;
+  std::vector<std::vector<Edge>> edge_lists_;
 };
 
 std::istream& operator>>(std::istream& in, Graph& gg) {
@@ -109,24 +82,40 @@ std::istream& operator>>(std::istream& in, Graph& gg) {
   return in;
 }
 
-std::string FindDistsToStart(Graph& gg, int start_v) {
-  gg.DistUpd(start_v, 0);
-  int curr_v = gg.GetNearestUnused();
-  while (gg.UnusedExists()) {
-    auto it = gg.GetNeighbours(curr_v).begin();
-    long long dist = gg.GetDist(curr_v);
-    for (; it != gg.GetNeighbours(curr_v).end(); ++it) {
-      if (dist + it->len < gg.GetDist(it->vert)) {
-        gg.DistUpd(it->vert, dist + it->len);
+std::vector<long long> FindDistsToStart(Graph& gg, int start_v) {
+  std::vector<long long> dists =
+          std::vector<long long>(gg.VerticesNum(), kMaxDist);
+  std::set<Node, Comp> unused;
+  for (int i = 0; i < gg.VerticesNum(); ++i) {
+    unused.insert(Node(i, dists[i]));
+  }
+  int vert = start_v;
+  unused.erase(Node(vert, dists[vert]));
+  dists[vert] = 0;
+  unused.insert(Node(vert, dists[vert]));
+  auto it = unused.begin();
+  Node nearest = *it;
+  unused.erase(it);
+  dists[nearest.vert] = nearest.dist;
+  int curr_v = nearest.vert;
+  while (!unused.empty()) {
+    auto vit = gg.GetNeighbours(curr_v).begin();
+    long long dist = dists[curr_v];
+    for (; vit != gg.GetNeighbours(curr_v).end(); ++vit) {
+      if (dist + vit->len < dists[vit->vert]) {
+        int vert = vit->vert;
+        unused.erase(Node(vert, dists[vert]));
+        dists[vert] = dist + vit->len;
+        unused.insert(Node(vert, dists[vert]));
       }
     }
-    curr_v = gg.GetNearestUnused();
+    it = unused.begin();
+    Node nearest = *it;
+    unused.erase(it);
+    dists[nearest.vert] = nearest.dist;
+    curr_v = nearest.vert;
   }
-  std::string ss;
-  for (int i = 0; i < gg.VerticesNum(); ++i) {
-    ss += std::to_string(gg.GetDist(i)) + " ";
-  }
-  return ss;
+  return dists;
 }
 
 int main() {
@@ -139,6 +128,9 @@ int main() {
     std::cin >> gg;
     int start_v;
     std::cin >> start_v;
-    std::cout << FindDistsToStart(gg, start_v) << '\n';
+    std::vector<long long> dists = FindDistsToStart(gg, start_v);
+    for (auto dist : dists) {
+      std::cout << dist << ' ';
+    }
   }
 }
